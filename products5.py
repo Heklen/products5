6d2eed2dd70b0936e48f28bef933233c509d8e9c
Helken <a9623943@gmail.com>
2021-05-31 12:14:16 +0800

    add else

---
+++ products5.py
@@ -0,0 +1,54 @@
+import os
+#讀取檔案
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename):#檢查檔案在不在
+	    print('yeah!')
+	    with open (filename, 'r', encoding='utf-8') as f:
+		    for line in f:
+			    if '商品,價格' in line:
+				    continue
+			    name, price = line.strip().split(',')
+			    products.append([name, price])
+		print(products)
+	else:
+		print('找不到檔案')
+	return products
+
+#使用者輸入
+def user_input(products):
+    while True:
+	    name = input('請輸入商品名稱:')
+	    if name == 'q':
+		    break
+	    price = input('請輸入商品價格:')
+	    price = int(price)
+	    products.append([name, price])
+    print(products)
+    return products
+
+#印出所有商品紀錄
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格是', p[1])
+
+    return products
+#寫入檔案
+def write_file(filename, products):
+    with open('products.csv', 'w', encoding='utf-8') as f:
+	    f.write('商品,價格\n')
+	    for p in products:
+	        f.write(p[0] + ',' + str(p[1]) + '\n')  
+
+
+def main():
+	if os.path.isfile(filename):檢查檔案在不在
+	    print('yeah!,有檔案')
+	    products = read_file('products.csv')
+	elde:
+	    print('找不到檔案')
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
